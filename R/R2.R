ab96a437c09287fe1ad1cbe823fa85f0f9f62091
Yijun Ma <mayijun1203@gmail.com>
2021-02-05 21:31:49 -0500

    update

--- R/R2.R
+++ R/R2.R
@@ -14,6 +14,7 @@ k = list(1, 2, 'a')
 
 
 # Read data
+data.frame()
 df = read.csv('C:/Users/mayij/Desktop/DOC/GITHUB/SKILLGH/R/psam_h36.csv')
 
 
@@ -27,12 +28,13 @@ summary(df)
 # Slicing
 df1 = df[1:10, 1:2]
 df1 = df[c(1, 3, 5), c('PUMA', 'BROADBND')]
-df1 = df['PUMA']
+df1 = df[c('PUMA', 'BROADBND')]
 df1 = df$PUMA
 
 
 # Filtering
 df2 = df[df['PUMA'] == 4004, ]
+df2 = subset(df, PUMA == 4004)
 df2 = filter(df, PUMA == 4004)
 df2 = df %>% filter(PUMA == 4004)
 
@@ -50,11 +52,19 @@ df4 = df %>% mutate(INCWGTP = ADJINC * WGTP)
 
 
 # Piping
+df5 = df %>% filter(PUMA == 4004) %>% select(PUMA, ADJINC, WGTP)
 df5 = df %>% filter(PUMA == 4004) %>% select(PUMA, ADJINC, WGTP) %>% mutate(INCWGTP = ADJINC * WGTP)
 
 
 # Group by and summarize
 df6 = df %>% group_by(PUMA) %>% summarize(WEIGHT = sum(WGTP))
+
+
+# Join
+df71=df %>% filter(PUMA == 4004) %>% select(SERIALNO, ADJINC)
+df72=df %>% filter(PUMA == 4004) %>% select(SERIALNO, WGTP)
+df7=merge(df71,df72,by='SERIALNO',all=T)
+df7=inner_join(df71,df72,by='SERIALNO')
 
 
 # For loop
@@ -66,7 +76,7 @@ for (i in 1:5) {
 
 # if else
 df6$PUMACAT = NaN
-for (i in 1:5) {
+for (i in 1:10) {
   if (df6[i, 'PUMA'] == 401) {
     df6[i, 'PUMACAT'] = 1
   }
