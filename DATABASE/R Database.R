9216df03e1c404c672005eeca6a8ac4c35d27bda
Yijun Ma <mayijun1203@gmail.com>
2021-06-20 14:38:57 -0400

    UPDATE

---
+++ DATABASE/R Database.R
@@ -0,0 +1,117 @@
+# SQLite
+library(RSQLite)
+con=dbConnect(SQLite(),'C:/Users/mayij/Desktop/test')
+dbListTables(con)
+
+
+df=read.csv('https://raw.githubusercontent.com/mayijun1203/SKILLGH/master/R/psam_h36.csv',
+            stringsAsFactors=F,
+            colClasses=c(PUMA='character',ST='character'))
+dbWriteTable(con,'pums',df)
+dbListTables(con)
+
+
+df=dbReadTable(con,'pums')
+
+
+df=dbGetQuery(con, 'SELECT SERIALNO,PUMA FROM pums LIMIT 100')
+
+
+dbDisconnect(con)
+
+
+
+
+
+
+# SpatiaLite
+library(RSQLite)
+library(sf)
+con=dbConnect(SQLite(),'C:/Users/mayij/Desktop/testspatial.sqlite')
+dbListTables(con)
+
+df=st_read('https://raw.githubusercontent.com/mayijun1203/SKILLGH/master/CARTO/subwayridership.geojson')
+df=st_set_crs(df,4326)
+st_write(df,con,'subway',driver='SQLite')
+
+dbListTables(con)
+
+
+df=st_read(con,'subway')
+plot(df)
+
+df=st_read(con,query='SELECT CplxID,geometry FROM subway LIMIT 100')
+plot(df)
+
+
+dbDisconnect(con)
+
+
+
+
+
+
+# PostgreSQL
+library(RPostgres)
+con=dbConnect(Postgres(),
+              dbname='postgres',
+              host='localhost',
+              port=5432,
+              user='postgres',
+              password='123456'
+              )
+dbListTables(con)
+
+
+df=read.csv('https://raw.githubusercontent.com/mayijun1203/SKILLGH/master/R/psam_h36.csv',
+            stringsAsFactors=F,
+            colClasses=c(PUMA='character',ST='character'))
+dbWriteTable(con,'pums',df)
+dbListTables(con)
+
+
+df=dbReadTable(con,'pums')
+
+
+df=dbGetQuery(con,'SELECT "SERIALNO","PUMA" FROM pums LIMIT 100')
+
+
+dbDisconnect(con)
+
+
+
+
+
+
+
+
+# PostGIS
+library(RSQLite)
+library(sf)
+con=dbConnect(Postgres(),
+              dbname='postgres',
+              host='localhost',
+              port=5432,
+              user='postgres',
+              password='123456')
+dbListTables(con)
+
+df=st_read('https://raw.githubusercontent.com/mayijun1203/SKILLGH/master/CARTO/subwayridership.geojson')
+df=st_set_crs(df,4326)
+st_write(df,con,'subway',driver='PostgreSQL')
+dbListTables(con)
+
+
+df=st_read(con,'subway')
+plot(df)
+
+df=st_read(con,query='SELECT "CplxID","geometry" FROM subway LIMIT 100')
+plot(df)
+
+
+dbDisconnect(con)
+
+
+
+
+
