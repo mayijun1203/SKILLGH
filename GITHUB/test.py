37aea45c430e5747bae12b7ff8d46822478a3200
Yijun Ma <mayijun1203@gmail.com>
2021-11-19 01:00:44 -0500

    autoupdate

--- GITHUB/test.py
+++ GITHUB/test.py
@@ -1,5 +1,28 @@
 import os
+import git
+import datetime
+import pytz
+import pandas as pd
 
-df=os.environ['t']
 
-print(df)
+
+user=os.environ['user']
+key=os.environ['key']
+
+
+
+
+path='C:/Users/mayij/Desktop/DOC/GITHUB/SKILLGH/'
+timestamp=datetime.datetime.now(pytz.timezone('US/Eastern')).strftime('%m/%d/%Y')
+df=pd.DataFrame()
+df.to_csv(path+'/test.csv')
+
+
+
+repo=git.Repo(path)
+repo.git.add('.')
+repo.index.commit('autoupdate')
+origin=repo.remote(name='origin')
+origin.push()
+
+
