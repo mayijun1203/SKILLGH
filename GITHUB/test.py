a8a452acd520a16eb9f10904fd95f8d6e0543990
Yijun Ma <mayijun1203@gmail.com>
2021-11-19 01:17:52 -0500

    update

--- GITHUB/test.py
+++ GITHUB/test.py
@@ -6,13 +6,6 @@ import pandas as pd
 
 
 
-# user=os.environ['user']
-# key=os.environ['key']
-
-
-
-
-# path='C:/Users/mayij/Desktop/DOC/GITHUB/SKILLGH/'
 path='./GITHUB/'
 timestamp=datetime.datetime.now(pytz.timezone('US/Eastern')).strftime('%m%d%Y')
 df=pd.DataFrame()
@@ -20,10 +13,10 @@ df.to_csv(path+'timestamp.csv')
 
 
 
-# repo=git.Repo(path)
-# repo.git.add('.')
-# repo.index.commit('autoupdate')
-# origin=repo.remote(name='origin')
-# origin.push()
+repo=git.Repo(path)
+repo.git.add('.')
+repo.index.commit('autoupdate')
+origin=repo.remote(name='origin')
+origin.push()
 
 
