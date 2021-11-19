08b3c63fc834cf1582ef4f20f6566a9b0adc45c7
Yijun Ma <mayijun1203@gmail.com>
2021-11-19 01:06:29 -0500

    update

--- GITHUB/test.py
+++ GITHUB/test.py
@@ -16,7 +16,7 @@ import pandas as pd
 path='./GITHUB/'
 timestamp=datetime.datetime.now(pytz.timezone('US/Eastern')).strftime('%m%d%Y')
 df=pd.DataFrame()
-df.to_csv(path+'/timestamp.csv')
+df.to_csv(path+'timestamp.csv')
 
 
 
